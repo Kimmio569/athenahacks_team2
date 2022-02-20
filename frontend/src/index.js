41784f305ab39b4b97db8c0f5d138e56aebd81a7
Jiyi Zhou <jiyi@Jiyis-MacBook-Pro.local>
2022-02-19 16:15:26 -0800

    navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './App.css';
+import { Menu } from 'antd';
+import { EnvironmentOutlined, PhoneOutlined, MoneyCollectOutlined, CommentOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="container">
-                <h1 className="center-align">
-                    Athena<br/>
-                    <span className="waves-effect waves-light btn">
-                        <i className="material-icons right">cloud</i>Hacks
-                    </span>
-                </h1>
-            </div>
-        );
-    }
+  state = {
+    current: 'mail',
+  };
+
+  handleClick = e => {
+    console.log('click ', e);
+    this.setState({ current: e.key });
+  };
+
+  render() {
+    const { current } = this.state;
+    return (
+      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item key="map" icon={<EnvironmentOutlined />}>
+          Crime Map
+        </Menu.Item>
+        <Menu.Item key="report" icon={<PhoneOutlined />}>
+          Report Crime
+        </Menu.Item>
+        <Menu.Item key="fund" icon={<MoneyCollectOutlined />}>
+          Go Fund me
+        </Menu.Item>
+        <Menu.Item key="forum" icon={<CommentOutlined />}>
+          Forum
+        </Menu.Item>
+      </Menu>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
