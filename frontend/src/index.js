b2ea59ab513d4990e99ca75e4427f902da1303fe
Jiyi Zhou <jiyi@Jiyis-MacBook-Pro.local>
2022-02-19 16:50:41 -0800

    d3 test

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
 import './App.css';
+import BarChart from './BarChart.js';
+
+import 'antd/dist/antd.css';
 import { Menu } from 'antd';
 import { EnvironmentOutlined, PhoneOutlined, MoneyCollectOutlined, CommentOutlined } from '@ant-design/icons';
 
@@ -9,7 +11,11 @@ const { SubMenu } = Menu;
 
 class App extends React.Component {
   state = {
-    current: 'mail',
+    current: 'map',
+    data: [12, 5, 6, 6, 9, 10],
+    width: 700,
+    height: 500,
+    id: 123
   };
 
   handleClick = e => {
@@ -20,20 +26,23 @@ class App extends React.Component {
   render() {
     const { current } = this.state;
     return (
-      <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-        <Menu.Item key="map" icon={<EnvironmentOutlined />}>
-          Crime Map
-        </Menu.Item>
-        <Menu.Item key="report" icon={<PhoneOutlined />}>
-          Report Crime
-        </Menu.Item>
-        <Menu.Item key="fund" icon={<MoneyCollectOutlined />}>
-          Go Fund me
-        </Menu.Item>
-        <Menu.Item key="forum" icon={<CommentOutlined />}>
-          Forum
-        </Menu.Item>
-      </Menu>
+      <div>
+        <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
+          <Menu.Item key="map" icon={<EnvironmentOutlined />}>
+            Crime Map
+          </Menu.Item>
+          <Menu.Item key="report" icon={<PhoneOutlined />}>
+            Report Crime
+          </Menu.Item>
+          <Menu.Item key="fund" icon={<MoneyCollectOutlined />}>
+            Go Fund me
+          </Menu.Item>
+          <Menu.Item key="forum" icon={<CommentOutlined />}>
+            Forum
+          </Menu.Item>
+        </Menu>
+        <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
+      </div>
     );
   }
 }
