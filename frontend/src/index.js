490a224c462e1e86be6bd91d7cbdc236b1f49c32
Jiyi Zhou <jiyi@Jiyis-MacBook-Pro.local>
2022-02-19 17:19:24 -0800

    router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,44 +4,59 @@ import './App.css';
 import BarChart from './BarChart.js';
 
 import 'antd/dist/antd.css';
-import { Menu } from 'antd';
+import { Menu, Layout } from 'antd';
 import { EnvironmentOutlined, PhoneOutlined, MoneyCollectOutlined, CommentOutlined } from '@ant-design/icons';
 
-const { SubMenu } = Menu;
-
+const { Content, Header } = Layout;
 class App extends React.Component {
   state = {
-    current: 'map',
+    menuTab: 'map',
     data: [12, 5, 6, 6, 9, 10],
     width: 700,
     height: 500,
     id: 123
   };
 
-  handleClick = e => {
+  components = {
+    'map': <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />,
+    'report': <div>Option 2</div>,
+    'fund': <div>Option 3</div>,
+    'forum': <div>Option 4</div>
+  };
+
+  handleMenuClick = e => {
     console.log('click ', e);
-    this.setState({ current: e.key });
+    this.setState({ menuTab: e.key });
   };
 
   render() {
     const { current } = this.state;
     return (
       <div>
-        <Menu onClick={this.handleClick} selectedKeys={[current]} mode="horizontal">
-          <Menu.Item key="map" icon={<EnvironmentOutlined />}>
-            Crime Map
-          </Menu.Item>
-          <Menu.Item key="report" icon={<PhoneOutlined />}>
-            Report Crime
-          </Menu.Item>
-          <Menu.Item key="fund" icon={<MoneyCollectOutlined />}>
-            Go Fund me
-          </Menu.Item>
-          <Menu.Item key="forum" icon={<CommentOutlined />}>
-            Forum
-          </Menu.Item>
-        </Menu>
-        <BarChart data={this.state.data} width={this.state.width} height={this.state.height} />
+        <Header>
+          <Menu onClick={this.handleMenuClick} selectedKeys={[current]} mode="horizontal">
+            <Menu.Item key="map" icon={<EnvironmentOutlined />}>
+              Crime Map
+            </Menu.Item>
+            <Menu.Item key="report" icon={<PhoneOutlined />}>
+              Report Crime
+            </Menu.Item>
+            <Menu.Item key="fund" icon={<MoneyCollectOutlined />}>
+              Go Fund me
+            </Menu.Item>
+            <Menu.Item key="forum" icon={<CommentOutlined />}>
+              Forum
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Layout>
+          <Content>{this.components[this.state.menuTab]}</Content>
+
+
+
+
+        </Layout>
+
       </div>
     );
   }
